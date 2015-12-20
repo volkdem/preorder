5d878412800f38910477bdae5827e956f00ae3cd
vadim.demidov <vad.demidov@yahoo.com>
2015-12-20 20:11:48 +0300

    Added jetty war autodeploy and JSON(Order) sent to server will save in root folder and responce to client as well

--- PreOrderServer/src/main/java/org/prototype/rest/server/PreOrder_Server_REST.java
+++ PreOrderServer/src/main/java/org/prototype/rest/server/PreOrder_Server_REST.java
@@ -1,6 +1,7 @@
 package org.prototype.rest.server;
 
 import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.prototype.model.Order;
 
 import javax.ws.rs.Consumes;
@@ -8,6 +9,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Vadim on 28.11.2015.
@@ -17,6 +22,8 @@ public class PreOrder_Server_REST {
 
     private Logger log = Logger.getLogger(PreOrder_Server_REST.class);
 
+    private List<Order> orders;
+
 
 
     @POST
@@ -24,16 +31,50 @@ public class PreOrder_Server_REST {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response processPreOrderData(Order order) {
 
+
+        if(isOrdersEmpty()) {
+            orders = new ArrayList<Order>();
+        }
+
+
+        if(isIncommingOrderEmpty(order)) {
+            return Response.status(500).entity("The -=Order=- sent to REST service is null").build();
+        }
+
+
+
         Order newOrder = order;
-        System.out.println(newOrder.toString());
+        System.out.println("Server internals: " + newOrder.toString());
 
-        if(newOrder.getOrderID().equals("E_123"))
-            return Response.status(200).entity(newOrder.toString()).build();
-        else
-            return Response.status(422).entity(newOrder.toString()).build();
+        String writeFileTo = "./order.json";
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(new File(writeFileTo), order);
+        } catch (IOException e) { e.printStackTrace(); }
 
+
+        return Response.status(200).entity("JSON :" + newOrder.toString() + " stored in " + writeFileTo).build();
     }
 
+
+
+    private boolean isIncommingOrderEmpty(Order order) {
+
+        if(order == null) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+    private boolean isOrdersEmpty() {
+        if(this.orders == null) {
+            return true;
+        }
+        return false;
+    }
 
 
 
