77959ed1aec0cd7ec5e02c879033c475e06931bf
Evgeny Volkov <es.volkov@yandex.ru>
2015-12-20 18:42:35 +0300

    Mobile client updates

--- mobile_client/Preordrer/src/main/java/com/preorder/preorder/OrderConfirmationActivity.java
+++ mobile_client/Preordrer/src/main/java/com/preorder/preorder/OrderConfirmationActivity.java
@@ -7,7 +7,6 @@ import android.widget.ListView;
 import com.preorder.preorder.model.Product;
 import com.preorder.preorder.model.ProductBin;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class OrderConfirmationActivity extends AppCompatActivity {
@@ -20,6 +19,7 @@ public class OrderConfirmationActivity extends AppCompatActivity {
         ListView orderListView = (ListView) findViewById( R.id.list_item );
         List< Product > productOrder = (List< Product >)getIntent().getSerializableExtra( ProductBinFragment.PRODUCT_ORDER_KEY );
         ProductBin productBin = (ProductBin) getIntent().getSerializableExtra( ProductBinFragment.BIN_KEY );
-        orderListView.setAdapter( new ProductListAdapter( productOrder ) );
+        orderListView.setAdapter( new ProductListAdapter( productOrder, productBin ) );
+
     }
 }
